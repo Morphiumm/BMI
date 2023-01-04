1e27de2f619270178d6df7c618c4aa339752f39b
ilias <iliasa.work@gmail.com>
2023-01-05 02:03:58 +0600

    0.1

---
+++ app/src/main/java/com/morphium/bmi/presentation/BmiDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.morphium.bmi.presentation
+
+import androidx.fragment.app.Fragment
+import com.morphium.bmi.R
+
+class BmiDetailsFragment: Fragment(R.layout.fragment_bmi_details) {
+}
---
+++ app/src/main/java/com/morphium/bmi/presentation/BmiResultFragment.kt
@@ -0,0 +1,7 @@
+package com.morphium.bmi.presentation
+
+import androidx.fragment.app.Fragment
+import com.morphium.bmi.R
+
+class BmiResultFragment: Fragment(R.layout.fragment_bmi_result) {
+}
